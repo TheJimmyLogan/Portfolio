cbe9dd74f552ce6ad093a086cb7995ed432e46f5
Эрик Э <rick-jimmy@mail.ru>
2022-05-29 21:28:26 +0300

    fixed vh

--- script.js
+++ script.js
@@ -19,3 +19,13 @@ typewriter
   .pauseFor(1500)
   .deleteChars(7)
   .start();
+
+  
+const resizeVh = () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+resizeVh();
+
+window.addEventListener('resize', resizeVh);
