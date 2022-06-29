2d803f9a1ad456e17d027d96227eacf99f3de94c
Эрик Э <rick-jimmy@mail.ru>
2022-06-30 00:09:39 +0300

    menu

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 var app = document.getElementById('type-writer');
 
-var typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 75,
-});
+if (app) {
+  var typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
+  });
 
-typewriter
-  .typeString('Web Developer')
-  .pauseFor(1500)
-  .deleteChars(13)
-  .typeString('Web Designer')
-  .pauseFor(1500)
-  .deleteChars(12)
-  .typeString('UI/UX Designer')
-  .pauseFor(1500)
-  .deleteChars(14)
-  .typeString('Dreamer')
-  .pauseFor(1500)
-  .deleteChars(7)
-  .start();
+  typewriter
+    .typeString('Web Developer')
+    .pauseFor(1500)
+    .deleteChars(13)
+    .typeString('Web Designer')
+    .pauseFor(1500)
+    .deleteChars(12)
+    .typeString('UI/UX Designer')
+    .pauseFor(1500)
+    .deleteChars(14)
+    .typeString('Dreamer')
+    .pauseFor(1500)
+    .deleteChars(7)
+    .start();
+}
 
 
 const resizeVh = () => {
